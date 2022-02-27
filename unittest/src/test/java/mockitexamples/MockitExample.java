c55412a80c923c7bb6c281f19d1c80a97e4d5a4d
butterflying dog <butterflyingdog@163.com>
2022-02-27 17:33:14 +0800

    Update unittest/src/test/java/mockitexamples/MockitExample.java

--- unittest/src/test/java/mockitexamples/MockitExample.java
+++ unittest/src/test/java/mockitexamples/MockitExample.java
@@ -5,7 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
-import java.utils.List;
+import java.util.List;
   
 public class MockitExample {
 
