d595aa2bc7db6ff8512e739e81a3ac7428229d27
butterflyingdog <butterflyingdog@163.com>
2022-03-03 12:56:55 +0000

    update

--- unittest/src/test/java/mockitexamples/MockitExampleTest.java
+++ unittest/src/test/java/mockitexamples/MockitExampleTest.java
@@ -17,6 +17,7 @@ public class MockitExampleTest {
         //模拟PersonDao对象
         mockDao = mock(PersonDao.class);
         when(mockDao.getPerson(1)).thenReturn(new Person(1, "Person1"));
+         when(mockDao.getPerson(2)).thenReturn(new Person(2, "Person2"));
         when(mockDao.update(isA(Person.class))).thenReturn(true);
 
         personService = new PersonService(mockDao);
@@ -24,20 +25,24 @@ public class MockitExampleTest {
 
     @Test
     public void testUpdate() throws Exception {
-        boolean result = personService.update(1, "new name");
+        boolean result = personService.update(2, "new name");
+        System.out.println("MockitExample.testUpdate flag0");
         assertTrue("must true", result);
+        System.out.println("MockitExample.testUpdate flag1");
         //验证是否执行过一次getPerson(1)
         verify(mockDao, times(1)).getPerson(eq(1));
+        System.out.println("MockitExample.testUpdate flag2");
         //验证是否执行过一次update
         verify(mockDao, times(1)).update(isA(Person.class));
+        System.out.println("MockitExample.testUpdate flag3");
     }
 
     @Test
     public void testUpdateNotFind() throws Exception {
-        boolean result = personService.update(2, "new name");
-        System.out.println("MockitExample flag0");
-        assertFalse("must true", result);
-        System.out.println("MockitExample flag1");
+        boolean result = personService.update(3, "new name");
+        System.out.println("MockitExample.testUpdateNotFind flag0, result="+result);
+        assertFalse("result should be false", result);
+        System.out.println("MockitExample.testUpdateNotFind flag1");
         //验证是否执行过一次getPerson(1)
         verify(mockDao, times(1)).getPerson(eq(1));
         System.out.println("MockitExample flag2");
