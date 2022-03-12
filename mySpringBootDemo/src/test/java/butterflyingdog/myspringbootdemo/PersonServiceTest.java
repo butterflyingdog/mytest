4b1f5308fee5533d448ec5837f70b7b650681d68
butterflyingdog <butterflyingdog@163.com>
2022-03-12 10:14:27 +0000

    update

--- mySpringBootDemo/src/test/java/butterflyingdog/myspringbootdemo/PersonServiceTest.java
+++ mySpringBootDemo/src/test/java/butterflyingdog/myspringbootdemo/PersonServiceTest.java
@@ -1,23 +1,33 @@
 package butterflyingdog.myspringbootdemo;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.context.junit4.SpringRunner;
 
+import myspringbootdemo.personmng.dao.PersonDao;
 import myspringbootdemo.personmng.service.PersonService;
  
  
-@SpringBootTest()
+@SpringBootTest(classes={myspringbootdemo.MySpringBootDemoApplication.class})
 public class PersonServiceTest {
+    
+    @Mock
+    private PersonDao personDao;
 
-    @Autowired
+    @InjectMocks
     private PersonService userService;
 
     @Test
     public void addUser() throws Exception {
+        
+        when(personDao.createUser("zhihao.miao")).thenReturn(1); 
+
         Assertions.assertEquals(Integer.valueOf(1),userService.addUser("zhihao.miao"));
         Assertions.assertEquals(Integer.valueOf(0),userService.addUser(null));
       
