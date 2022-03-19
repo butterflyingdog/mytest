faf25788bbc4aa771d5c4e82bf04657a1e6cef33
butterflyingdog <butterflyingdog@163.com>
2022-03-19 15:13:12 +0000

    update

--- mySpringBootDemo/src/main/java/myspringbootdemo/emailmng/bean/EMail.java
+++ mySpringBootDemo/src/main/java/myspringbootdemo/emailmng/bean/EMail.java
@@ -4,7 +4,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
 import javax.persistence.Entity;
-//import org.hibernate.annotations.Entity;
+
  
 import org.springframework.data.annotation.Id;
 
@@ -17,21 +17,19 @@ public class EMail {
  
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long emailId;
  
-    private String name;
+    private String emailSubject;
  
-    private String url;
+    private String emailContent;
  
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", url='" + url + '\'' +
+        return "Email{" +
+                "emailId=" + emailId +
+                ", emailSubject='" + emailSubject + '\'' +
+                ", emailContent='" + emailContent + '\'' +
                 '}';
     }
 }
-————————————————
-版权声明：本文为CSDN博主「java干货」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/dandandeshangni/article/details/108544261
+ 
