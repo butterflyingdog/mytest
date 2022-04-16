0b25d7eb5cfa177bcc4f1b086f5a790e24f9d144
butterflyingdog <butterflyingdog@163.com>
2022-04-16 15:41:47 +0000

    update

--- mySpringBootDemo/src/test/java/butterflyingdog/myspringbootdemo/TestPersonController_by_SpringExtension.java
+++ mySpringBootDemo/src/test/java/butterflyingdog/myspringbootdemo/TestPersonController_by_SpringExtension.java
@@ -5,12 +5,12 @@ import   org.mockito.Mockito;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
--- mySpringBootDemo/src/test/java/butterflyingdog/myspringbootdemo/TestPersonService_by_SpringExtension.java
+++ mySpringBootDemo/src/test/java/butterflyingdog/myspringbootdemo/TestPersonService_by_SpringExtension.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+
 //import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Configuration;
