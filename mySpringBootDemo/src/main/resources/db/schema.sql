e7f6ac41068284befe73c2ed6cbbbee9d0f12643
butterflyingdog <butterflyingdog@163.com>
2022-04-29 23:19:04 +0800

    update

--- mySpringBootDemo/src/main/resources/db/schema.sql
+++ mySpringBootDemo/src/main/resources/db/schema.sql
@@ -1,4 +1,6 @@
-create TABLE USER_INF if not exist(
+drop TABLE USER_INF;  
+create TABLE USER_INF --if not exist
+(
     person_id INTEGER PRIMARY KEY,
     person_name VARCHAR(100), 
     person_sex VARCHAR(2)
