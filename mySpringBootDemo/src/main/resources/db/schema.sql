8beef631f373c35d811430867738cd86656ae573
butterflyingdog <butterflyingdog@163.com>
2022-05-01 13:43:39 +0000

    update

--- mySpringBootDemo/src/main/resources/db/schema.sql
+++ mySpringBootDemo/src/main/resources/db/schema.sql
@@ -1,7 +1,7 @@
---drop TABLE USER_INF;  
-create TABLE USER_INF if not exists
+drop TABLE USER_INF if exists;  
+create TABLE USER_INFOMATION --if not exists
 (
     person_id INTEGER PRIMARY KEY,
     person_name VARCHAR(100), 
     person_sex VARCHAR(2)
-    );
+);
--- mySpringBootDemo/src/test/java/myspringbootdemo/servicemngtest/SpringBootTest_with_TestRestTemplate.java
+++ mySpringBootDemo/src/test/java/myspringbootdemo/servicemngtest/SpringBootTest_with_TestRestTemplate.java
@@ -12,13 +12,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
-import myspringbootdemo.personmngt.dao.PersonDao;
+
 
 @SpringBootTest(classes={myspringbootdemo.MySpringBootDemoApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBootTest_with_TestRestTemplate {
 
-    //@MockBean
-    //PersonDao personDao;
 
 
     //这个对象是运行在web环境的时候加载到spring容器中
@@ -27,7 +25,7 @@ public class SpringBootTest_with_TestRestTemplate {
 
     @BeforeEach
     public void setup(){
-   //     Mockito.when(personDao.createUser("ning")).thenReturn( Integer.valueOf(1));
+
     }
     @Test
     public void testNotExistUrl_should_404(){
