861816885c82ac1a032502dbb99361725b6fe9f2
butterflyingdog <butterflyingdog@163.com>
2022-05-02 14:43:17 +0000

    update

--- mySpringBootDemo/src/main/java/myspringbootdemo/MySpringBootDemoApplication.java
+++ mySpringBootDemo/src/main/java/myspringbootdemo/MySpringBootDemoApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableOpenApi
+
 public class MySpringBootDemoApplication {
 
 	public static void main(String[] args) {
