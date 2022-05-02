ae89047299546c3b0f57ea5178fa714c09ffbb33
butterflyingdog <butterflyingdog@163.com>
2022-05-02 15:36:53 +0000

    update

---
+++ mySpringBootDemo/src/test/java/myspringbootdemo/swaggertest/SwaggerTest.java
@@ -0,0 +1,38 @@
+package myspringbootdemo.swaggertest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+
+@SpringBootTest(classes={myspringbootdemo.MySpringBootDemoApplication.class},webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+public class SwaggerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+    private static final String url = "/v3/api-docs";
+
+ 
+
+    @Test
+    private void executeInvoke(Object body){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+         
+        HttpEntity<HashMap<String, Object>> request = new HttpEntity(body, headers);
+        String result = restTemplate.getForObject(url ,String.class  );
+        System.out.println(result);
+        //Assertions.assertEquals("SUCCESS:{\"address\":\"广东深圳\",\"name\":\"小芳\",\"age\":18}",result);
+
+    }
+}
