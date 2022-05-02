ef3cd21675cb12b0a2d0d16b3f8b32aacacca817
butterflyingdog <butterflyingdog@163.com>
2022-05-02 11:36:46 +0000

    update

--- mySpringBootDemo/src/main/java/myspringbootdemo/requestbodydemo/RequestBodyController.java
+++ mySpringBootDemo/src/main/java/myspringbootdemo/requestbodydemo/RequestBodyController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+
 import com.alibaba.fastjson.JSONObject;
 //import org.json.JSONObject;
 
 @RestController
 @RequestMapping("/RequestBodyController")
+
+@Api
 
 public class RequestBodyController {
     @RequestMapping(value = "/json", method = RequestMethod.POST)
