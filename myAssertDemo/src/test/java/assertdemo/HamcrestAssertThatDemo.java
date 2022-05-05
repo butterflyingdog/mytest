aac54b3f500551e499f2adfb5cbd152ea749d6e4
butterflyingdog <butterflyingdog@163.com>
2022-05-05 08:23:26 +0000

    update

--- myAssertDemo/src/test/java/assertdemo/HamcrestAssertThatDemo.java
+++ myAssertDemo/src/test/java/assertdemo/HamcrestAssertThatDemo.java
@@ -4,13 +4,12 @@ import java.util.*;
 
 import static org.junit.Assert.assertEquals;
  
-
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
  
 import static org.hamcrest.CoreMatchers.*; 
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat; 
 
 import org.junit.jupiter.api.Test; 
@@ -35,12 +34,15 @@ class HamcrestAssertionDemo {
  /*
        * 一般匹配符
        */
+     Integer  testedNumber = Integer.valueof("89.4");
 // allOf匹配符表明如果记下来的所有条件必须都成立测试才能通过，相当于“与”（&&）
 assertThat( testedNumber , allOf( greaterThan(8) , lessThan(16) ));
 // anyOf匹配符表名如果接下来的条件只要有一个成立，则测试通过，相当于“或”（||）
 assertThat( testedNumber , anyOf( greaterThan(16) , lessThan(8) ));
 // anything匹配符表名无论什么条件，永远为true
 assertThat( testedNumber , anything() );
+
+String testedString = "hh";
 // is匹配符表名如果前面待测的object等于后面的object，则测试通过
 assertThat( testedString , is( "developerWorks" ));
 // not匹配符和is匹配符正好相反，表明前面待测的object不等于后面给出的object,则测试通过
@@ -59,14 +61,14 @@ assertThat( testedString , startsWith( "developerWorks" ) );
 // equalTo匹配符表明如果测试的testedValue等于expectedValue，则测试通过，equalTo可以测试数值之间，字符串之间和对象之间是否相等，相当于Object的equals方法
 assertThat( testedString , equalTo( expectedValue ));
 // equalToIgnoringCase匹配符表明如果测试的字符串testedString在忽略大小写的情况下等于“developerWorks”,则测试通过
-assertThat( testedString , equalToIgnoringCase( “developerWorks” ) );
+assertThat( testedString , equalToIgnoringCase( "developerWorks" ) );
 // equalToIgnoringWhiteCase匹配符表明如果测试的字符串testedString在忽略头尾的任意空格的情况下等于“developerWorks”,则测试通过，注意：字符串中间的空格不能被忽略
-assertThat( testedString , equalToIgnoringWhiteCase( “developerWorks” ) );
+assertThat( testedString , equalToIgnoringWhiteCase( "developerWorks") );
 
 /*
  * 数值相关匹配符
  */
-
+Double d = Double.valueOf("83.4");
 // 测试的浮点型数testedDouble在20±0.5
 assertThat( testedDouble , closeTo( 20. , 0.5 ));
 // 测试的浮点型数testedDouble > 16.0
@@ -81,10 +83,12 @@ assertThat( testedDouble , lessThanOrEqualTo( 16.0 ));
 /*
  * collection相关匹配符
  */
+Map mapObject = new HashMap();
 // hasEntry匹配符表明如果测试的Map对象mapObject含有一个键值为“key”对应的元素值为“value”的Entry，则测试通过
 assertThat( mapObject , hasEntry( "key" , "value" ) );
 // hasItem匹配符表明如果测试的迭代对象iterableObject含有元素“elemenet”项，则测试通过
-assertThat( iterableObject , hasItem( “element” ) );
+Map iterableObject = new HashMap();
+assertThat( iterableObject , hasItem( "element" ) );
 // hasKey匹配符表明测试的Map对象mapObject含有键值"key"则测试通过
 assertThat( mapObject , hasKey( "key" ) );
 // hasValue匹配符表明如果测试的Map对象mapObject含有元素值“value”，则测试通过
@@ -98,8 +102,10 @@ assertThat( mapObject , hasValue( "value" ) );
  // 想要判断某个字符串s是否含有个子字符串“developer”或“Worker”中间的一个
  // JUnit 4.4 以前版本：assertTrue(s.indexOf("developer")>-1 || s.indexOf("Worker")>-1);
  // JUnit 4.4 匹配符anyOf 表示任何一个条件满足则成立，类似于逻辑或 “||”，匹配符containsString 表示是否含有参数子字符串
+ String s = "ggg";
  assertThat(s , anyOf(containsString("developer"),containsString("Worker")));
  // 联合匹配符not和equalTo表示“不等于”
+ String something = "gg";
  assertThat(something , not(equqlTt("developer")));
  // 联合匹配符not和containsString表示“不包含子字符串”
  assertThat(something , not(containsString("Worker")));
