f5ad03d9d3fa10d798bc7b9b9561ce8581d91fde
butterflyingdog <butterflyingdog@163.com>
2022-05-14 15:10:46 +0000

    update

--- myMockitoDemo/src/test/java/mockitexamples/Mockito_Init_At_Setup.java
+++ myMockitoDemo/src/test/java/mockitexamples/Mockito_Init_At_Setup.java
@@ -15,31 +15,31 @@ import java.util.*;
 //@DisplayName("Use Mockito.mock")
 public class Mockito_Init_At_Setup {
 
-    private PersonDao     mockDao;
-    private PersonService personService;
+    private PersonDao     mockedObject;
+    private PersonService serviceUsingMockedObject;
 
     @Before
     public void setUp() throws Exception {
         //模拟PersonDao对象
-        mockDao = Mockito.mock(PersonDao.class);
-        Mockito.when(mockDao.getPerson(1)).thenReturn(new Person(1, "Person1"));
-        Mockito.when(mockDao.getPerson(2)).thenReturn(new Person(2, "Person2"));
-        Mockito.when(mockDao.update(Mockito.isA(Person.class))).thenReturn(true);
+        mockedObject = Mockito.mock(PersonDao.class);
+        Mockito.when(mockedObject.getPerson(1)).thenReturn(new Person(1, "Person1"));
+        Mockito.when(mockedObject.getPerson(2)).thenReturn(new Person(2, "Person2"));
+        Mockito.when(mockedObject.update(Mockito.isA(Person.class))).thenReturn(true);
 
-        personService = new PersonService(mockDao);
+        serviceUsingMockedObject = new PersonService(mockedObject);
     }
 
     @Test
     public void testUpdate() throws Exception {
-        boolean result = personService.update(2, "new name");
+        boolean result = serviceUsingMockedObject.update(2, "new name");
         System.out.println("Mockito_Init_At_Setup.testUpdate flag0");
         assertTrue("must true", result);
         System.out.println("Mockito_Init_At_Setup.testUpdate flag1");
         //验证是否执行过一次getPerson(1)
-        Mockito.verify(mockDao, Mockito.times(1)).getPerson(Mockito.eq(2));
+        Mockito.verify(mockedObject, Mockito.times(1)).getPerson(Mockito.eq(2));
         System.out.println("Mockito_Init_At_Setup.testUpdate flag2");
         //验证是否执行过一次update
-        Mockito.verify(mockDao, Mockito.times(1)).update(Mockito.isA(Person.class));
+        Mockito.verify(mockedObject, Mockito.times(1)).update(Mockito.isA(Person.class));
         System.out.println("Mockito_Init_At_Setup.testUpdate flag3");
     }
 
@@ -47,18 +47,18 @@ public class Mockito_Init_At_Setup {
     public void verifyFail() throws Exception {
        
        
-        assertNull("result should be null", personService.getPerson(3));
+        assertNull("result should be null", serviceUsingMockedObject.getPerson(3));
         System.out.println("Mockito_Init_At_Setup.noMockedMethodDemo flag1");
         //验证是否执行过一次getPerson(1)
-        Mockito.verify(mockDao, Mockito.times(1)).getPerson(Mockito.eq(3));
+        Mockito.verify(mockedObject, Mockito.times(1)).getPerson(Mockito.eq(3));
         System.out.println("Mockito_Init_At_Setup.noMockedMethodDemo flag2");
         //验证是否执行过一次update
-        Mockito.verify(mockDao, Mockito.never()).update(Mockito.isA(Person.class));
+        Mockito.verify(mockedObject, Mockito.never()).update(Mockito.isA(Person.class));
         System.out.println("Mockito_Init_At_Setup.noMockedMethodDemo flag3");
     }
 
     @Test
     public void noMockedMethod() throws Exception {
-        assertNull ( "should be null", personService.getPerson(3 ));
+        assertNull ( "should be null", serviceUsingMockedObject.getPerson(3 ));
     }
 }
