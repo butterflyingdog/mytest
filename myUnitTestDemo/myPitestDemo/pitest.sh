8333784e4cd9a6eda63f0f29d9a4a1e8910d36fc
bbb <bbbb@163.com>
2022-07-04 21:58:05 +0800

    update

--- myUnitTestDemo/myPitestDemo/pitest.sh
+++ myUnitTestDemo/myPitestDemo/pitest.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
+mvn clean org.jacoco:jacoco-maven-plugin:0.8.8:prepare-agent  site
 mvn org.pitest:pitest-maven:mutationCoverage site
--- myUnitTestDemo/myPitestDemo/src/test/java/pitestdemo/MyPitestDemo.java
+++ myUnitTestDemo/myPitestDemo/src/test/java/pitestdemo/MyPitestDemo.java
@@ -8,6 +8,12 @@ public class MyPitestDemo {
     @Test
     public void testGreaterThan() {
 
-        Assert.assertFalse(Calculator.greaterThan(5, 6));
+        Assert.assertFalse(new Calculator().greaterThan(5, 6));
+    }
+
+    @Test
+    public void testLesserThan() {
+
+        Assert.assertTrue(Calculator.greaterThan(7, 6));
     }
 }
