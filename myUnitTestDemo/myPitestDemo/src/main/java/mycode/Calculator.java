c0a0d2a79e093f5887259f044835f0d694c341e8
bbb <bbbb@163.com>
2022-07-05 15:36:16 +0800

    update

--- myUnitTestDemo/myPitestDemo/src/main/java/mycode/Calculator.java
+++ myUnitTestDemo/myPitestDemo/src/main/java/mycode/Calculator.java
@@ -3,7 +3,10 @@ package mycode;
 public class Calculator {
 
     public static boolean greaterThan(int a, int b) {
-        return a > b;
+        if (a > b)
+            return true;
+        else
+            return false;
     }
 
 }
