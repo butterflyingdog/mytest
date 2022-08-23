bd00e7a265cd644c522c6dab39ee3af10735973d
bbb <bbbb@163.com>
2022-08-23 22:29:21 +0800

    update

--- myUnitTestDemo/myPitestDemo/pitest.sh
+++ myUnitTestDemo/myPitestDemo/pitest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mvn  clean  -f pom-goodTestCases.xml
 #mvn org.jacoco:jacoco-maven-plugin:0.8.8:prepare-agent  
-mvn   test org.pitest:pitest-maven:mutationCoverage     -f pom-goodTestCases.xml
-mvn   test org.pitest:pitest-maven:mutationCoverage     -f pom-badTestCases.xml
+mvn   test      -f pom-goodTestCases.xml
+mvn   test    -f pom-badTestCases.xml
