448384544b4a60ea884a3f1469d14254b742d482
bbb <bbbb@163.com>
2022-08-26 22:54:19 +0800

    update

--- myUnitTestDemo/myPitestDemo/src/test/java/pitestdemo/cucumber_junit4/Pitest_Cucumber_Junit4.java
+++ myUnitTestDemo/myPitestDemo/src/test/java/pitestdemo/cucumber_junit4/Pitest_Cucumber_Junit4.java
@@ -9,12 +9,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {
-        "pretty:target/cucumber/pretty-report.log",
-        "html:target/cucumber/report.html",
-        "json:target/cucumber/report.json",
-        "junit:target/cucumber/report.xml",
-        "pretty"
-}, features = { "src/test/resources/PiTest.feature" })
+                "pretty:target/cucumber/pretty-report.log",
+                "html:target/cucumber/report.html",
+                "json:target/cucumber/report.json",
+                "junit:target/cucumber/report.xml",
+                "pretty"
+}, features = { "src/test/resources/PiTest.feature" }, glue = { "pitestdemo.cucumber_junit4" })
 
 public class Pitest_Cucumber_Junit4 {
 
