85ec521acb5f66a2a916f47c465f09be66b1ddbb
bbb <bbbb@163.com>
2022-08-27 14:40:20 +0800

    update

--- myUnitTestDemo/myPitestDemo/src/test/java/pitestdemo/cucumber_junit5/Pitest_CucumberStep_Junit5.java
+++ myUnitTestDemo/myPitestDemo/src/test/java/pitestdemo/cucumber_junit5/Pitest_CucumberStep_Junit5.java
@@ -5,9 +5,22 @@ import mycode.Calculator;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Pitest_CucumberStep_Junit5 {
-    @When("PiTestDemoStep")
-    public void PiTestDemoStep() {
+
+    @When("test_A_LessThan_B_And_B_isNot_0_should_plus")
+    public void test_A_LessThan_B_And_B_isNot_0_should_plus() {
 
         assertEquals(Calculator.greaterThan(5, 6), 11);
+    }
+
+    @When("test_A_GreatThan_B_And_B_isNot_0_should_divide")
+    public void test_A_GreatThan_B_And_B_isNot_0_should_divide() {
+
+        assertEquals(Calculator.greaterThan(9, 6), 1);
+    }
+
+    @When("test_A_GreatThan_B_And_B_is_0_should_A")
+    public void test_A_GreatThan_B_And_B_is_0_should_A() {
+
+        assertEquals(Calculator.greaterThan(9, 0), 9);
     }
 }
--- myUnitTestDemo/myPitestDemo/src/test/java/pitestdemo/cucumber_junit5/Pitest_Cucumber_Junit5.java
+++ myUnitTestDemo/myPitestDemo/src/test/java/pitestdemo/cucumber_junit5/Pitest_Cucumber_Junit5.java
@@ -1,17 +1,16 @@
 package pitestdemo.cucumber_junit5;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {
-        "pretty:target/cucumber/pretty-report.log",
-        "html:target/cucumber/report.html",
-        "json:target/cucumber/report.json",
-        "junit:target/cucumber/report.xml",
-        "pretty"
-}, features = { "src/test/resources/PiTest.feature" })
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("./")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class Pitest_Cucumber_Junit5 {
 
 }
