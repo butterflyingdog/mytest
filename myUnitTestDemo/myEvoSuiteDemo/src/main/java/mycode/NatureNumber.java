256b39ec591dc68d9f379758699bd5987e7ba2ea
bbb <bbbb@163.com>
2022-09-15 08:50:30 +0800

    update

---
+++ myUnitTestDemo/myEvoSuiteDemo/src/main/java/mycode/NatureNumber.java
@@ -0,0 +1,11 @@
+package mycode;
+
+public class NatureNumber {
+
+    public static boolean isNatureNumber(int a) {
+        if (a >= 0)
+            return true;
+        else
+            return false;
+    }
+}
